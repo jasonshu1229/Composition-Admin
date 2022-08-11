18687595c6e743afdeda4de5bea0fb60716cd644
梁书豪 <liangshuhao@liangshuhaodeiMac.local>
2022-08-11 20:38:27 +0800

    feat: 配置代码规范，配置vue.config，搭建vue-router-v4

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,22 @@
 const { defineConfig } = require('@vue/cli-service');
+const path = require('path');
+
 module.exports = defineConfig({
-	transpileDependencies: true
+	transpileDependencies: true,
+	// 配置方式一： LCI提供的属性
+	outputDir: './build',
+	// 配置方式二： 和webpack属性完全一致，最后会进行合并
+	// configureWebpack: {
+	// 	resolve: {
+	// 		alias: {
+	// 			components: '@/components'
+	// 		}
+	// 	}
+	// }
+	configureWebpack: (config) => {
+		config.resolve.alias = {
+			'@': path.resolve(__dirname, 'src'),
+			components: '@/components'
+		};
+	}
 });
