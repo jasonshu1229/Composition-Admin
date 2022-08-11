78a6f300e1c4c2482151c3bff7ffac234a57711e
liangshuhao <shuhao1229@163.com>
2022-08-11 23:22:20 +0800

    feat((vuex)): vuex集成简单配置

---
+++ src/store/index.ts
@@ -0,0 +1,14 @@
+import { createStore } from 'vuex';
+
+const store = createStore({
+	state: () => {
+		return {
+			name: 'lsh'
+		};
+	},
+	mutations: {},
+	getters: {},
+	actions: {}
+});
+
+export default store;
