dda7e90f51d401d040dee22a08bdab65203ff17f
liangshuhao <shuhao1229@163.com>
2022-08-19 12:44:34 +0800

    fix: fix了一个刷新页面自动跳转到notFound页面bug

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,12 @@ import 'normalize.css';
 import './assets/css/index.less';
 import * as ElementPlusIconsVue from '@element-plus/icons-vue';
 
-// import shRequest from './service';
-
-// 全局引入 1)
+// 1) 全局引入
 // import ElementPlus from 'element-plus';
 // import 'element-plus/dist/index.css';
+
+// 2) 全局注册
+// app.use(ElementPlus);
 
 import router from './router';
 import store from './store/index';
@@ -16,48 +17,13 @@ import { setupStore } from '@/store';
 
 const app = createApp(App);
 
-// app.use(router);
-// app.use(store);
-// 全局注册 2)
-// app.use(ElementPlus);
+app.use(store);
 
-// app.mount('#app');
-
-app.use(router).use(store).mount('#app');
 setupStore(); // 防止页面刷新，vuex数据丢失
+app.use(router);
+
+app.mount('#app');
 
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
 	app.component(key, component);
 }
-
-// shRequest.request({
-// 	url: '/home/multidata',
-// 	method: 'GET',
-// 	interceptors: {
-// 		requestInterceptor: (config) => {
-// 			console.log('单独请求的config');
-// 			return config;
-// 		},
-// 		responseInterceptor: (res) => {
-// 			console.log('单独响应的response');
-// 			return res;
-// 		}
-// 	}
-// });
-
-// interface DataType {
-// 	data: any;
-// 	returnCode: string;
-// 	success: boolean;
-// }
-
-// shRequest
-// 	.get<DataType>({
-// 		url: '/home/multidata',
-// 		showLoading: false
-// 	})
-// 	.then((res) => {
-// 		console.log(res.data);
-// 		console.log(res.returnCode);
-// 		console.log(res.success);
-// 	});
