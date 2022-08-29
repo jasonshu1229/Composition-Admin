4b706e0b31189eab360e7d85750e81b54d78089c
liangshuhao <shuhao1229@163.com>
2022-08-30 00:09:14 +0800

    feat: base-echart最基本的封装规则

---
+++ src/base-ui/echart/src/base-echart.vue
@@ -0,0 +1,33 @@
+<template>
+	<div class="base-echart">
+		<div ref="echartDivRef" :style="{ width: width, height: height }"></div>
+	</div>
+</template>
+
+<script lang="ts" setup>
+import { ref, onMounted, defineProps, withDefaults } from 'vue';
+import * as echarts from 'echarts';
+import { EChartsOption } from 'echarts';
+
+const echartDivRef = ref<HTMLElement>();
+
+const props = withDefaults(
+	defineProps<{
+		width?: string;
+		height?: string;
+		option: EChartsOption;
+	}>(),
+	{
+		width: '100%',
+		height: '360px'
+	}
+);
+
+onMounted(() => {
+	const echartInstance = echarts.init(echartDivRef.value!);
+
+	echartInstance.setOption(props.option);
+});
+</script>
+
+<style scoped lang="less"></style>
--- src/views/main/analysis/dashboard/dashboard.vue
+++ src/views/main/analysis/dashboard/dashboard.vue
@@ -14,7 +14,9 @@
 
 		<el-row :gutter="10" class="content-row">
 			<el-col :span="12">
-				<sh-card title="分类商品的销量"></sh-card>
+				<sh-card title="分类商品的销量">
+					<base-echart :option="option" />
+				</sh-card>
 			</el-col>
 			<el-col :span="12">
 				<sh-card title="不同商品的收藏"></sh-card>
@@ -27,15 +29,39 @@
 import { defineComponent } from 'vue';
 
 import { useStore } from '@/store';
+
 import ShCard from '@/base-ui/card/index';
+import BaseEchart from '@/base-ui/echart';
 
 export default defineComponent({
 	name: 'dashboard',
-	components: { ShCard },
+	components: { ShCard, BaseEchart },
 	setup() {
 		const store = useStore();
 		store.dispatch('dashboard/getDashboardDataAction');
-		return {};
+
+		const option = {
+			xAxis: {
+				type: 'category',
+				data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+			},
+			yAxis: {
+				type: 'value'
+			},
+			series: [
+				{
+					data: [120, 200, 150, 80, 70, 110, 130],
+					type: 'bar',
+					showBackground: true,
+					backgroundStyle: {
+						color: 'rgba(180, 180, 180, 0.2)'
+					}
+				}
+			]
+		};
+		return {
+			option
+		};
 	}
 });
 </script>
